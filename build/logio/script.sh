9d3b6e5bc5728c39da8ce4d531bc43b3612551f3
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-26 16:55:46 +0300

    Update script.sh

--- build/logio/script.sh
+++ build/logio/script.sh
@@ -6,13 +6,13 @@ fi
 
 if [[ "${LOGIO_HARVESTER}" == "yes" ]]
     then
-        FILE=./harvester_conf.sh	
+        FILE=./harvester_conf.sh
         if test -f "$FILE"
-            then	
-                echo "$FILE exist"	
+            then
+                echo "$FILE exist"
             else
                 curl https://raw.githubusercontent.com/ros-kamach/log.io_alpine/master/openshift/harvester_conf.sh \
-                --output /harvester_conf.sh
+                --output ./harvester_conf.sh
         fi
      bash ./harvester_conf.sh
      log.io-harvester

15739e4db4aba188c34c56a5b2ec1f60cf336b84
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-26 16:36:24 +0300

    Update harvester_conf.sh

--- openshift/harvester_conf.sh
+++ openshift/harvester_conf.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+touch /home/logio/logs/error.log
+touch /home/logio/logs/access.log
+rm /home/logio/.log.io/harvester.conf
+cat <<EOF | sudo tee -a /home/logio/.log.io/harvester.conf
+exports.config = {
+  nodeName: "node",
+  logStreams: {
 
+    error: [
+          "/home/logio/logs/error.log"
+        ],
+
+    access: [
+          "/home/logio/logs/access.log"
+        ],
+
+},
+
+  server: {
+    host: 'logio-server.thunder.svc',
+    port: 28777
+  }
+}
+EOF

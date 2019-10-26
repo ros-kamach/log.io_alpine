8c073dcd30e48271317ad21e867cd7614c23dff5
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-26 16:57:01 +0300

    Update harvester_conf.sh

--- openshift/harvester_conf.sh
+++ openshift/harvester_conf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir /home/logio/logs
 touch /home/logio/logs/error.log
 touch /home/logio/logs/access.log
-rm /home/logio/.log.io/harvester.conf
-cat <<EOF | sudo tee -a /home/logio/.log.io/harvester.conf
+rm /home/logio/.log.io/harvester.conf 2>/dev/null
+cat <<EOF | tee -a /home/logio/.log.io/harvester.conf
 exports.config = {
   nodeName: "node",
   logStreams: {

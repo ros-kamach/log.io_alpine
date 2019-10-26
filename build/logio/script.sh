e8fae4f7ee2296b8e28ea5e20865a24bf400b56d
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-26 21:55:04 +0300

    Update script.sh

--- build/logio/script.sh
+++ build/logio/script.sh
@@ -4,6 +4,20 @@ if [[ "${LOGIO_SERVER}" == "yes" ]]
         log.io-server
 fi
 
+if [[ "${OPENSHIFT_CLI}" == "yes" ]]
+    then
+    apk --no-cache add ca-certificates
+    curl -L https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /etc/apk/keys/sgerrand.rsa.pub
+    curl -L https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.28-r0/glibc-2.28-r0.apk -o glibc-2.28-r0.apk
+    apk add glibc-2.28-r0.apk
+    curl -L https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz --output \
+    /openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz
+    tar -xf /openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz -C /
+    mv /openshift-origin-client-tools-v3.9.0-191fece-linux-64bit/oc /usr/local/bin
+    rm -rf /openshift-origin-client-tools-*
+    apk del ca-certificates
+fi
+
 if [[ "${LOGIO_HARVESTER}" == "yes" ]]
     then
         FILE=./harvester_conf.sh

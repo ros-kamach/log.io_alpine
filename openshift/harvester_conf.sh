4e924bf3bec4008141bdbd0e75ce5bab2ac2fd8d
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-28 11:22:32 +0200

    Update harvester_conf.sh

--- openshift/harvester_conf.sh
+++ openshift/harvester_conf.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 mkdir /home/logio/logs
-touch /home/logio/logs/error.log
-touch /home/logio/logs/access.log
+# touch /home/logio/logs/error.log
+# touch /home/logio/logs/access.log
 rm /home/logio/.log.io/harvester.conf 2>/dev/null
+oc logs -f jenkins-1-rgfbt --tail=-1 -n jenkins-ci | tee /home/logio/logs/jenkins-1-rgfbt.log >  /dev/null 2>&1 &
+oc logs -f mysql-1-llmjp --tail=-1 -n thunder | tee /home/logio/logs/mysql-1-llmjp.log >  /dev/null 2>&1 &
 cat <<EOF | tee -a /home/logio/.log.io/harvester.conf
 exports.config = {
-  nodeName: "node",
+  nodeName: "thunder",
   logStreams: {
 
-    error: [
-          "/home/logio/logs/error.log"
-        ],
-
-    access: [
-          "/home/logio/logs/access.log"
+    "mysql-1-llmjp": [
+          "/home/logio/logs/mysql-1-llmjp.log"
         ],
 
 },
+  nodeName: "jenkins-ci",
+  logStreams: {
+
+    "jenkins-1-rgfbt": [
+          "/home/logio/logs/jenkins-1-rgfbt.log"
+        ],
+
+}
 
   server: {
     host: 'logio-server.thunder.svc',

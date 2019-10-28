1c83f89d73c18002649566e1096348d9f2892a10
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-28 15:20:08 +0200

    Update harvester_conf.sh

--- openshift/harvester_conf.sh
+++ openshift/harvester_conf.sh
@@ -5,36 +5,37 @@ mkdir /home/logio/logs
 rm /home/logio/.log.io/harvester.conf 2>/dev/null
 oc logs -f jenkins-1-rgfbt --tail=-1 -n jenkins-ci | tee /home/logio/logs/jenkins-1-rgfbt.log >  /dev/null 2>&1 &
 oc logs -f mysql-1-llmjp --tail=-1 -n thunder | tee /home/logio/logs/mysql-1-llmjp.log >  /dev/null 2>&1 &
-# cat <<EOF | tee -a /home/logio/.log.io/harvester.conf
-# exports.config = {
-#   nodeName: "thunder",
-#   logStreams: {
-
-#     "mysql-1-llmjp": [
-#           "/home/logio/logs/mysql-1-llmjp.log"
-#         ],
-
-# },
-#   nodeName: "jenkins-ci",
-#   logStreams: {
-
-#     "jenkins-1-rgfbt": [
-#           "/home/logio/logs/jenkins-1-rgfbt.log"
-#         ],
-
-# },
-
-#   server: {
-#     host: 'logio-server.thunder.svc',
-#     port: 28777
-#   }
-# }
-# EOF
 cat <<EOF | tee -a /home/logio/.log.io/harvester.conf
 exports.config = {
-server: {
-  host: 'logio-server.thunder.svc',
-  port: 28777
+  nodeName: "jenkins-ci",
+  logStreams: {
+    jenkins-1-rgfbt: ["./logs/jenkins-1-rgfbt.log"],
+    mysql-1-llmjp: ["./logs/mysql-1-llmjp.log"],
+},
+
+  server: {
+    host: 'logio-server.thunder.svc',
+    port: 28777
+  }
+}
+exports.config = {
+  nodeName: "thunder",
+  logStreams: {
+    mysql-1-llmjp: ["./logs/mysql-1-llmjp.log"],
+    jenkins: ["./logs/jenkins-1-rgfbt.log"],
+},
+
+  server: {
+    host: 'logio-server.thunder.svc',
+    port: 28777
   }
 }
 EOF
+# cat <<EOF | tee -a /home/logio/.log.io/harvester.conf
+# exports.config = {
+# server: {
+#   host: 'logio-server.thunder.svc',
+#   port: 28777
+#   }
+# }
+# EOF
